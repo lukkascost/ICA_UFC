72eeaa66575b38d73ef9303e179f6b372694f6e0
Lucas Costa <lucas.costa@lit.ifce.edu.br>
2018-09-01 14:20:41 -0300

    graphics histogram att per class

--- HOMEWORK1/DataAnalisys.py
+++ HOMEWORK1/DataAnalisys.py
@@ -4,20 +4,25 @@ import matplotlib.pyplot as plt
 index = np.loadtxt("../glass.data.txt", usecols=0, delimiter=",", dtype=int)
 attributes = np.loadtxt("../glass.data.txt", usecols=[x for x in range(1, 10)], delimiter=",")
 labels = np.loadtxt("../glass.data.txt", usecols=-1, delimiter=",", dtype=int)
-# for i in range(9):
-#     plt.clf()
-#     plt.scatter(index, attributes[:, i], marker=".")
-#     plt.savefig("FIGURE/sc_att-{:02d}_cls-{}.png".format(i + 1, "ALL"), dpi=100)
-#
-# for i in range(9):
-#     for j in [1, 2, 3, 5, 6, 7]:
-#         plt.clf()
-#         x = index[labels == j]
-#         y = attributes[x-1, i]
-#         plt.scatter(x, y, marker=".")
-#         plt.savefig("FIGURE/sc_att-{:02d}_cls-{}.png".format(i + 1, j), dpi=100)
-
 for i in range(9):
     plt.clf()
-    plt.hist(attributes[:, i], 100)
-    plt.savefig("FIGURE/hist_att-{:02d}_cls-{}.png".format(i + 1, "ALL"), dpi=100)
+    plt.scatter(index, attributes[:, i], marker=".")
+    plt.savefig("FIGURE/sc_att-{:02d}_cls-{}.png".format(i + 1, "ALL"), dpi=100)
+
+for i in range(9):
+    for j in [1, 2, 3, 5, 6, 7]:
+        plt.clf()
+        x = index[labels == j]
+        y = attributes[x-1, i]
+        plt.scatter(x, y, marker=".")
+        plt.savefig("FIGURE/sc_att-{:02d}_cls-{}.png".format(i + 1, j), dpi=100)
+
+
+for i in range(9):
+    for j in [1, 2, 3, 5, 6, 7]:
+        plt.clf()
+        x = index[labels == j]
+        y = attributes[x-1, i]
+        plt.hist(y, 100)
+        plt.savefig("FIGURE/hist_att-{:02d}_cls-{}.png".format(i + 1,j), dpi=100)
+
