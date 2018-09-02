9b8b21d6e07ceb152211d3808ba7a0f27c6c4c9f
lukkascost <lucas.costa@lit.ifce.edu.br>
2018-09-02 12:48:55 -0300

    topic 1 and 2 results and graphics done.

--- HOMEWORK1/DataAnalisys.py
+++ HOMEWORK1/DataAnalisys.py
@@ -4,25 +4,30 @@ import matplotlib.pyplot as plt
 index = np.loadtxt("../glass.data.txt", usecols=0, delimiter=",", dtype=int)
 attributes = np.loadtxt("../glass.data.txt", usecols=[x for x in range(1, 10)], delimiter=",")
 labels = np.loadtxt("../glass.data.txt", usecols=-1, delimiter=",", dtype=int)
+
+""" Unconditional analysis: generate histogram for each attribute
+    independent of they class.
+"""
 for i in range(9):
     plt.clf()
-    plt.scatter(index, attributes[:, i], marker=".")
-    plt.savefig("FIGURE/sc_att-{:02d}_cls-{}.png".format(i + 1, "ALL"), dpi=100)
+    plt.hist(attributes[:, i], 100)
+    plt.savefig("FIGURE/hist_att-{:02d}_cls-{}.png".format(i + 1, "ALL"), dpi=100)
 
+
+""" class conditional analysis: generate histogram for each attribute in each class.
+"""
 for i in range(9):
     for j in [1, 2, 3, 5, 6, 7]:
         plt.clf()
         x = index[labels == j]
-        y = attributes[x-1, i]
-        plt.scatter(x, y, marker=".")
-        plt.savefig("FIGURE/sc_att-{:02d}_cls-{}.png".format(i + 1, j), dpi=100)
-
-
-for i in range(9):
-    for j in [1, 2, 3, 5, 6, 7]:
-        plt.clf()
-        x = index[labels == j]
-        y = attributes[x-1, i]
+        y = attributes[x - 1, i]
         plt.hist(y, 100)
-        plt.savefig("FIGURE/hist_att-{:02d}_cls-{}.png".format(i + 1,j), dpi=100)
+        plt.savefig("FIGURE/hist_att-{:02d}_cls-{}.png".format(i + 1, j), dpi=100)
 
+
+""" Unconditional bi-variate analysis
+"""
+
+
+""" Unconditional multi-variate analysis
+"""
--- HOMEWORK1/Metrics.py
+++ HOMEWORK1/Metrics.py
@@ -5,17 +5,40 @@ index = np.loadtxt("../glass.data.txt", usecols=0, delimiter=",", dtype=int)
 attributes = np.loadtxt("../glass.data.txt", usecols=[x for x in range(1, 10)], delimiter=",")
 labels = np.loadtxt("../glass.data.txt", usecols=-1, delimiter=",", dtype=int)
 
+""" Unconditional analysis Mean, std and skewness results.
+"""
 resultTable = np.zeros((3, 9))
+strres = ""
 for i in range(9):
     resultTable[0, i] = np.mean(attributes[:, i])
     resultTable[1, i] = np.std(attributes[:, i])
     resultTable[2, i] = sp.skew(attributes[:, i])
-
-## Show results uncoditional per attribute
-strres = ""
-for i in range(9):
     strres += "Attribute {:02d}: {:.04f} &\t\t{:.04f} &\t\t{:.04f}\n".format(i + 1, resultTable[0, i],
                                                                              resultTable[1, i], resultTable[2, i])
 print strres
 
+""" class conditional analysis Mean, std and skewness results per class.
+"""
+resultTable = np.zeros((7, 3, 9))
+for j in [1, 2, 3, 5, 6, 7]:
+    for i in range(9):
+        data = attributes[labels == j, i]
+        resultTable[j - 1, 0, i] = np.mean(data)
+        resultTable[j - 1, 1, i] = np.std(data)
+        resultTable[j - 1, 2, i] = sp.skew(data)
+
+for j in [1, 2, 3, 5, 6, 7]:
+    strres = " {} ".format(j)
+    for i in range(9):
+        strres += "&{:.04f}".format(resultTable[j - 1, 0, i])
+    strres += "\\\\ "
+    print strres
+
+
+""" Unconditional bi-variate analysis
+"""
+
+
+""" Unconditional multi-variate analysis
+"""
 
