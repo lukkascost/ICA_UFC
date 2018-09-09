e41b17ef35e27567f3b4cee131059a2d46145902
lukkascost <lucas.costa@lit.ifce.edu.br>
2018-09-09 16:13:50 -0300

    all vs all and pca analysis

--- HOMEWORK1/Metrics.py
+++ HOMEWORK1/Metrics.py
@@ -1,10 +1,13 @@
 import numpy as np
 import scipy.stats as sp
 import matplotlib.pyplot as plt
+from matplotlib.mlab import PCA
+from numpy import linalg as LA
 
 index = np.loadtxt("../glass.data.txt", usecols=0, delimiter=",", dtype=int)
 attributes = np.loadtxt("../glass.data.txt", usecols=[x for x in range(1, 10)], delimiter=",")
 labels = np.loadtxt("../glass.data.txt", usecols=-1, delimiter=",", dtype=int)
+
 
 """ Unconditional analysis Mean, std and skewness results.
 """
@@ -47,5 +50,18 @@ for i in range(9):
     strres += "\\\\"
 print strres
 
+
 """ Unconditional multi-variate analysis
 """
+mu = attributes.mean(axis=0)
+sigma = attributes.std(axis=0)
+attributes = (attributes - mu)/sigma
+
+resultPca = PCA(attributes, standardize=False)
+plt.clf()
+result = np.dot(attributes, resultPca.Wt)
+print resultPca.fracs
+for i in range(9):
+    plt.scatter(index, resultPca.Y[:, i], label="Attribute {}".format(i+1))
+plt.legend()
+plt.show()
