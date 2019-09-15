b40ae75c6300266e8d663b1bdc625b00b652d745
lukkascost <lucas.costa@lit.ifce.edu.br>
2019-09-15 18:33:20 -0300

    Artifitial dataset and T3 perceptron

--- T3/Dts1_1.py
+++ T3/Dts1_1.py
@@ -3,32 +3,44 @@ import numpy as np
 
 from MachineLearn.Classes import Experiment, DataSet, Data
 from T3.Perceptron import Layered_perceptron
+import matplotlib.pyplot as plt
 
 COLOR = cm.rainbow(np.linspace(0, 1, 5))
 learning_rate = 0.01
-epochs = 20000
+epochs = 1000
 
 oExp = Experiment()
 
 oDataSet = DataSet()
-base = np.loadtxt("Datasets/iris.data", usecols=range(4), delimiter=",")
-classes = np.loadtxt("Datasets/iris.data", dtype=object, usecols=-1, delimiter=",")
+base = np.loadtxt("Datasets/artifitial1.data", usecols=range(2), delimiter=",")
+classes = np.loadtxt("Datasets/artifitial1.data", dtype=object, usecols=-1, delimiter=",")
 
 for x, y in enumerate(base):
     oDataSet.add_sample_of_attribute(np.array(list(np.float32(y)) + [classes[x]]))
 oDataSet.attributes = oDataSet.attributes.astype(float)
-oDataSet.normalize_data_set()
+# oDataSet.normalize_data_set()
 for j in range(20):
     print(j)
-    oData = Data(2, 31, samples=50)
-    oData.random_training_test_by_percent([50, 50, 50], 0.8)
-    perc = Layered_perceptron(learning_rate)
+    oData = Data(len(oDataSet.labelsNames), 31, samples=50)
+    oData.random_training_test_by_percent([50, 50, 50], 0.75)
+    perc = Layered_perceptron(learning_rate, len(oDataSet.labelsNames))
     perc.train(oDataSet.attributes[oData.Training_indexes], oDataSet.labels[oData.Training_indexes], epochs)
     oData.model = perc
-    oData.confusion_matrix = np.zeros((2, 2))
+    oData.confusion_matrix = np.zeros((len(oDataSet.labelsNames), len(oDataSet.labelsNames)))
     for i in oData.Testing_indexes:
         data = np.matrix(np.hstack(([-1], oDataSet.attributes[i]))).T
-        oData.confusion_matrix[int(oDataSet.labels[i]), perc.predict(data)] += 1
+        predicted = perc.predict(data)
+        oData.confusion_matrix[int(oDataSet.labels[i]), predicted] += 1
+        plt.scatter(oDataSet.attributes[i,0],oDataSet.attributes[i,1], color=COLOR[int(oDataSet.labels[i])])
+    for k1 in range(150):
+        print("K ", k1)
+        for k2 in range(30):
+            x1 = k1 / 50
+            x2 = k2 / 10
+            result = perc.predict(np.array([[-1], [x1], [x2]]))
+            plt.scatter(x1, x2, color=COLOR[result])
+    print(oData)
+    plt.show()
     oDataSet.append(oData)
 oExp.add_data_set(oDataSet,
                   description="  Experimento iris  PS 20 realizaçoes.".format())
--- T3/Perceptron.py
+++ T3/Perceptron.py
@@ -2,34 +2,45 @@ import numpy as np
 
 
 class Layered_perceptron():
-    def __init__(self, learning_rate):
+    def __init__(self, learning_rate, nclass):
         self.w = []
         self.wt = []
         self.et = []
         self.lr = learning_rate
+        self.nclass = nclass
 
     def train(self, att, labels, epochs):
         self.wt = []
         self.et = []
-        self.w = np.random.random((att.shape[1] + 1, 1))
+        self.w = np.random.random((self.nclass, att.shape[1] + 1))
         self.wt.append(self.w)
         data = np.hstack((att, labels))
         for ep in range(epochs):
-            erro = 0
+            erro = np.zeros((self.nclass, 1))
             np.random.shuffle(data)
             labels = data[:, -1]
             cpdata = data[:, :-1]
             for i in range(data.shape[0]):
                 X = np.matrix(np.hstack(([-1], cpdata[i]))).T
-                y = self.predict(X)[0, 0]
-                e = labels[i] - y
-                self.w = self.w + (self.lr * e * X)
-                erro += abs(e)
+                y = self.predict_intern(X)
+                y[np.argmax(y)] = 1
+                y[y != 1] = 0
+                labels_new = np.zeros((self.nclass, 1))
+                labels_new[int(labels[i])] = 1
+                e = labels_new - y
+                self.w = self.w + (self.lr * e * X.T)
+                erro += np.abs(e)
             self.et.append(erro)
-            if erro < 5:
+            if max(erro) == 0:
                 break
         print("EPOCA: ", ep + 1, "ERRO", erro, "W", self.w)
 
-    def predict(self, x):
-        u = self.w.T * x
+    def predict_intern(self, x):
+        u = self.w * x
         return u
+
+    def predict(self, x):
+        u = self.w * x
+        if (sum(u) > 1):
+            pass
+        return np.argmax(u)
